fd3527a8bc34e5352bbb38bcd72a215bc404db55
abavoil <17646791+abavoil@users.noreply.github.com>
2025-12-16 15:42:55 +0100

    Refactor codebase: Fix dependencies, translate comments, and improve structure

--- src/KEEP.jl
+++ src/KEEP.jl
@@ -1,5 +1,22 @@
 module KEEP
 
-greet() = print("Hello World!")
+const DEFAULT_TOLERANCE = 1e-9
+
+include("PointMassPara.jl")
+include("TorqueFunction.jl")
+include("Integrate.jl")
+include("PointMass10.jl")
+include("PointMass4.jl")
+include("LimitCycle.jl")
+include("SteadyState.jl")
+include("Visualisation.jl")
+include("Optimization.jl")
+
+#=
+No exports from base module.
+Use `using keep.PointMassPara` for exports of submodules
+`import keep.PointMass10 as PM10` for aliasing submodule name
+and `import keep.LimitCycle: TAU0` for importing a specific function/constant (exported or not by the submodule)
+=#
 
 end
